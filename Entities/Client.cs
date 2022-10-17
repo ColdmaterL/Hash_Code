d48907ee1cd8fa17148d138d2d59d72c88ecbcea
ColdmaterL <67527025+ColdmaterL@users.noreply.github.com>
2022-10-17 18:34:35 -0300

    Add files via upload

---
+++ Entities/Client.cs
@@ -0,0 +1,27 @@
+namespace TestProj3HashCode.Entities{
+    class Client{
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client(string name, string email){
+            Name = name;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Client)){
+                return false;
+            }
+            else{
+                Client other = obj as Client;
+                return Email.Equals(other.Email);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using TestProj3HashCode.Entities;
+
+namespace TestProj3HashCode{
+    class Program{
+        static void Main(string[] args){
+            string path;
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            System.Console.Write("Enter file name: ");
+            path = Console.ReadLine()!;
+            try{
+                using(StreamReader sr = File.OpenText(path)){
+                    while(!sr.EndOfStream){
+                        string[] line = sr.ReadLine()!.Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
+                    }
+                    System.Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch(IOException e){
+                System.Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
